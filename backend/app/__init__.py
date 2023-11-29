b2645872da7671565d0a0361fdfc5b47ffc69cb9
Cashel <cashel@utexas.edu>
2023-11-29 16:45:56 -0600

    Register ml blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,8 +13,10 @@ def create_app():
 
     from app.apis.user_api import user_bp
     from app.apis.image_api import image_bp
+    from app.apis.ml_api import ml_bp
 
     app.register_blueprint(user_bp, url_prefix='/api/users')
     app.register_blueprint(image_bp, url_prefix='/api/images')
+    app.register_blueprint(ml_bp, url_prefix='/api/ml')
 
     return app
