7c2d5ddf628ed95c28714e2b666ae072af559c93
Cashel <cashel@utexas.edu>
2023-12-15 14:35:46 -0600

    Remove image batching

--- backend/app/services/ml_service.py
+++ backend/app/services/ml_service.py
@@ -9,8 +9,6 @@ class MLService:
         self.img_width = 224
         self.img_height = 224
         self.model = tf.keras.models.load_model('app/static/serialized_models/dermavision_model_attempt_1')
-        # with open('app/static/serialized_models/dermavision_model_attempt_1.pkl', 'rb') as file:
-        #     self.model = pickle.load(file)
 
     def process_image(self, img_path):
         image = tf.io.read_file(img_path)
@@ -19,20 +17,11 @@ class MLService:
         image = tf.image.resize(image, size=[IMG_WIDTH, IMG_HEIGHT])
         image = tf.expand_dims(image, 0)
         return image
-    
-    def unbatchify(self, data):
-        images = []
-        labels = []
-        for image, label in data.unbatch().as_numpy_iterator():
-            images.append(image)
-            labels.append(label)
-        return images, labels
 
     def predict(self, image_path):
         try:
             processed_image = self.process_image(image_path)
             prediction = self.model.predict(processed_image)
-            # label_values = self.unbatchify(prediction)[1]
             return prediction[0].tolist()
 
         except Exception as e:
