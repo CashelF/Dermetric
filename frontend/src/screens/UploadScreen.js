c87c1af5231d3434563185c348af6c359ad3c34e
carine <carineharb3@gmail.com>
2024-02-26 18:22:10 -0600

    Add loading spinner

--- frontend/src/screens/UploadScreen.js
+++ frontend/src/screens/UploadScreen.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { Dimensions, View, Image, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Dimensions, View, Image, Text, StyleSheet, Platform, TouchableOpacity, ActivityIndicator } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import colors from '../../assets/colors/colors';
 import { useNavigation } from '@react-navigation/native';
+import ClipLoader from "react-spinners/ClipLoader";
 
 // Constants and utility functions
 const SERVER_URL = 'https://api.cashel.dev';
@@ -22,6 +23,7 @@ const base64ToBlob = (base64, mimeType) => {
 
 const UploadScreen = () => {
   const navigation = useNavigation();
+  const [isUploading, setIsUploading] = useState(false); 
 
   const requestPermissionAndPickImage = async () => {
     // Request permission (if not already granted)
@@ -65,39 +67,55 @@ const UploadScreen = () => {
     return formData;
   };
 
-  const handleUpload = async () => {
-    try {
-      const pickerResult = await requestPermissionAndPickImage();
-      if (!pickerResult) {
-        return;
+    const handleUpload = async () => {
+      try {
+        const pickerResult = await requestPermissionAndPickImage();
+        if (!pickerResult) {
+          return;
+        }
+        setIsUploading(true); 
+        
+        const formData = createFormData({
+          uri: pickerResult.assets[0].uri,
+          type: 'image/jpeg', // Adjust based on actual image type
+          name: 'photo.jpg', // Adjust based on actual image name or use a generated name
+        });
+  
+        const response = await fetch(`${SERVER_URL}/api/ml/predict`, {
+          method: 'POST',
+          body: formData,
+        });
+  
+        if (!response.ok) {
+          throw new Error('Something went wrong with the API call');
+        }
+  
+        const result = await response.json();
+        navigation.navigate('ResultScreen', { resultData: result });
+      } catch (error) {
+        console.error('Error during image upload: ', error);
+        alert('Error during image upload. Please try again.');
       }
-  
-      const formData = createFormData({
-        uri: pickerResult.assets[0].uri,
-        type: 'image/jpeg', // Adjust based on actual image type
-        name: 'photo.jpg', // Adjust based on actual image name or use a generated name
-      });
-  
-      const response = await fetch(`${SERVER_URL}/api/ml/predict`, {
-        method: 'POST',
-        body: formData,
-      });
-  
-      if (!response.ok) {
-        throw new Error('Something went wrong with the API call');
-      }
-  
-      const result = await response.json();
-      navigation.navigate('ResultScreen', { resultData: result });
-    } catch (error) {
-      console.error('Error during image upload: ', error);
-      alert('Error during image upload. Please try again.');
-    }
-  };  
-  
-  
+      finally {
+        setIsUploading(false); 
+      } 
+    };  
+
+
   return (
     <View style={styles.container}>
+      
+      {isUploading && (
+        <View style={styles.loaderContainer}> 
+          <ClipLoader
+          color={colors.accent} 
+          loading={isUploading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          />
+        </View>
+      )} 
       <View style={styles.imageAndEllipseContainer}>
         <View style={styles.ellipse} />
         <Text style={styles.title}>Dermetric.</Text>
@@ -107,9 +125,11 @@ const UploadScreen = () => {
       <Text style={styles.description}>
         Use our image classification machine learning algorithms to test for skin disorders
       </Text>
-      <TouchableOpacity style={styles.button} onPress={handleUpload}>
-        <Text style={{ color: 'white', fontSize: 18, fontFamily: 'Nunito-Bold', fontWeight: '700' }}>Upload Image</Text>
-      </TouchableOpacity>
+      {!isUploading && ( 
+        <TouchableOpacity style={styles.button} onPress={handleUpload}>
+          <Text style={{ color: 'white', fontSize: 18, fontFamily: 'Nunito-Bold', fontWeight: '700' }}>Upload Image</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -169,6 +189,13 @@ const styles = StyleSheet.create({
         height: 50,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    loaderContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 999, 
     },
 });
 
